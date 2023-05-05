72ca316a9764913e769cab417d8db4f6314b165b
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-05 22:15:41 +0800

    feat: proper rendering of chat responses

--- src/helpers/constants/book-data.ts
+++ src/helpers/constants/book-data.ts
@@ -41,4 +41,128 @@ export const bookData = `
     </book>
 </content>
 </url>
+<url>
+  <loc>https://books.toscrape.com/catalogue/category/books/romance_4/index.html</loc>
+  <content>
+    <book>
+      <url>https://books.toscrape.com/catalogue/pride-and-prejudice_432/index.html</url>
+      <desc>A classic romance novel by Jane Austen</desc>
+      <price>12.99</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/me-before-you-me-before-you-1_977/index.html</url>
+      <desc>A contemporary romance novel by Jojo Moyes</desc>
+      <price>7.99</price>
+      <availability>in stock</availability>
+    </book>
+  </content>
+</url>
+<url>
+  <loc>https://books.toscrape.com/catalogue/category/books/horror_12/index.html</loc>
+  <content>
+    <book>
+      <url>https://books.toscrape.com/catalogue/ghost-story_141/index.html</url>
+      <desc>A horror novel by Peter Straub</desc>
+      <price>20.39</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/the-shining_926/index.html</url>
+      <desc>A classic horror novel by Stephen King</desc>
+      <price>39.56</price>
+      <availability>in stock</availability>
+    </book>
+  </content>
+</url>
+<url>
+<loc>https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html</loc>
+<content>
+    <book>
+    <url>https://books.toscrape.com/catalogue/the-tattooist-of-auschwitz_964/index.html</url>
+    <desc>A novel by Heather Morris based on the real-life story of Lale Sokolov, a Jewish prisoner at Auschwitz who becomes the tattooist responsible for inking identification numbers on his fellow prisoners.</desc>
+    <price>54.23</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>https://books.toscrape.com/catalogue/the-nightingale_977/index.html</url>
+    <desc>A novel by Kristin Hannah about two sisters living in France during World War II who join the resistance movement against the German occupation.</desc>
+    <price>54.95</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+
+<url>
+<loc>https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html</loc>
+<content>
+    <book>
+    <url>https://books.toscrape.com/catalogue/the-hunger-games-the-hunger-games-1_19/index.html</url>
+    <desc>The first book in a trilogy by Suzanne Collins set in a dystopian future where teenagers are forced to compete in a televised fight to the death.</desc>
+    <price>57.73</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>https://books.toscrape.com/catalogue/divergent-divergent-1_968/index.html</url>
+    <desc>The first book in a trilogy by Veronica Roth about a society divided into factions based on personality traits, and the teenage protagonist who doesn't fit neatly into any one group.</desc>
+    <price>56.76</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+<url>
+  <loc>https://books.toscrape.com/catalogue/category/books/fantasy_15/index.html</loc>
+  <content>
+    <book>
+      <url>https://books.toscrape.com/catalogue/a-game-of-thrones-a-song-of-ice-and-fire-1_947/index.html</url>
+      <desc>The first book in George R.R. Martin's epic series, A Song of Ice and Fire.</desc>
+      <price>43.33</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/a-clash-of-kings-a-song-of-ice-and-fire-2_986/index.html</url>
+      <desc>The second book in George R.R. Martin's epic series, A Song of Ice and Fire.</desc>
+      <price>49.18</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/a-storm-of-swords-a-song-of-ice-and-fire-3_981/index.html</url>
+      <desc>The third book in George R.R. Martin's epic series, A Song of Ice and Fire.</desc>
+      <price>55.53</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/a-feast-for-crows-a-song-of-ice-and-fire-4_937/index.html</url>
+      <desc>The fourth book in George R.R. Martin's epic series, A Song of Ice and Fire.</desc>
+      <price>18.97</price>
+      <availability>in stock</availability>
+    </book>
+    <book>
+      <url>https://books.toscrape.com/catalogue/a-dance-with-dragons-a-song-of-ice-and-fire-5_768/index.html</url>
+      <desc>The fifth book in George R.R. Martin's epic series, A Song of Ice and Fire.</desc>
+      <price>51.04</price>
+      <availability>in stock</availability>
+    </book>
+  </content>
+</url>
+<url>
+  <loc>https://books.toscrape.com/catalogue/dracula_590/index.html</loc>
+  <content>
+    <book>
+      <title>Dracula</title>
+      <author>Bram Stoker</author>
+      <publisher>Penguin Classics</publisher>
+      <publication_date>1992-01-01</publication_date>
+      <language>English</language>
+      <format>Paperback</format>
+      <pages>432</pages>
+      <dimensions>12.9 x 19.8 x 2.6 cm</dimensions>
+      <weight>308 g</weight>
+      <price>7.49</price>
+      <availability>in stock</availability>
+      <description>Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It introduced the character of Count Dracula, and established many conventions of subsequent vampire fantasy. The novel tells the story of Dracula's attempt to move from Transylvania to England so that he may find new blood and spread the undead curse.</description>
+    </book>
+  </content>
+</url>
+
 `;
